f1f0bb83a8ce68a9af20be6fabb262e80db47a3a
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-11-19 19:16:07 +0100

    📈 working on the cartPage  📈

--- app/(root)/cart/page.tsx
+++ app/(root)/cart/page.tsx
@@ -4,6 +4,7 @@ import { Button } from '@/components/ui/button';
 import { RootState } from '@/store/store';
 import Image from 'next/image';
 import Link from 'next/link';
+import { it } from 'node:test';
 import React from 'react'
 import { useSelector } from 'react-redux';
 
@@ -49,10 +50,36 @@ const Cart = () => {
       {items.length > 0 && (
         <div className='md:w-4/5 w-[95%] mx-auto grid grid-cols-1 lg:grid-cols-6 gap-12'>
           {/* Cart Items */}
-          <div className='rounded-lg shadow-md overflow-hidden xl:col-span-4'>
+          <div className='rounded-lg shadow-md overflow-hidden lg:col-span-4'>
             <h1 className='p-4 text-xl sm:text-2xl md:text-3xl font-bold text-white bg-blue-700'>
                 Your Cart ({totalQuantity} Items)
             </h1>
+            {items.map((item) => {
+              return (
+                <div key={item.id}>
+                  <div className='flex pb-6 mt-2 p-5 border-b-[1.5px] border-opacity-25 border-gray-700 items-center space-x-10'>
+                    <div>
+                      <Image
+                        src={item.image}
+                        alt={item.title}
+                        width={180}
+                        height={180}
+                      />
+                    </div>
+                    <div>
+                      <h1 className='md:text-xl text-base font-bold text-black'>{item.title}</h1>
+                      <h1 className='md:text-lg text-sm font-semibold'>Category : {item.category}</h1>
+                      <h1 className='md:text-2xl text-lg font-bold text-blue-950'>${item.price}</h1>
+                      <h1 className='md:text-lg text-sm font-semibold'>Quantity : {item.quantity}</h1>
+                      <div className='flex items-center mt-4 space-x-2'>
+                        <Button className='flex items-center mt-4 space-x-2'>Add More</Button>
+                        <Button variant={"destructive"}>Remove</Button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              )
+            })}
           </div>
         </div>
       )}
